aa5ad445356b0a921ec6cf3a72b6930274f57b26
Vikram Thyagarajan <vikramthyagarajan92@gmail.com>
2023-10-28 06:50:57 +0530

    feat: added the default error page

---
+++ app/src/routes/+error.svelte
@@ -0,0 +1,6 @@
+<div
+	class="h-full w-full overflow-hidden flex flex-col items-center justify-center bg-background-800"
+>
+	<h1 class="mt-[100px] text-white-0 text-4xl">Something went wrong</h1>
+	<a class="mt-[50px] text-white-0 px-10 py-3 bg-primary-800 rounded-3xl" href="/">Go back home</a>
+</div>
